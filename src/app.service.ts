847e7032873f7cab5e3ff5752b1b40b92d33a2f7
Robert Isaac <robert.isaac@live.com>
2020-04-12 02:26:57 +0200

    parse lazy loaded modules as well

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,21 +8,53 @@ const {JSDOM} = jsdom;
 
 @Injectable()
 export class AppService {
+
+  dom: any;
+  private site: string;
+
   async parseSite(site: string): Promise<Endpoint[]> {
-    const main = await this.getScript(site, /main.*\.js/);
-    const mainCode = await this.getData(main);
-    const allEndpoints = mainCode.matchAll(/this\.http(Client)?\.(.+?)\((.*?)\)[;.}]/g);
+    this.site = site;
     const endpoints: Endpoint[] = [];
+    const main = await this.getScript(/main.*\.js/);
+    const mainUrl = this.getFullUrl(main);
+    const mainCode = await this.getData(mainUrl);
+    const allEndpoints = mainCode.matchAll(/this\.http(Client)?\.(.+?)\((.*?)\)[;.}]/g);
     for (const endpoint of allEndpoints) {
       endpoints.push({method: endpoint[2], parameters: endpoint[3]});
+    }
+    const runtime = await this.getScript(/runtime.*\.js/);
+    const runtimeUrl = this.getFullUrl(runtime);
+    const runtimeCode = await this.getData(runtimeUrl);
+    const modulesCode = runtimeCode.match(/\[e]\|\|e\)\+"(.+?)"\+({.+?})/);
+    if (modulesCode) {
+      const prefix = modulesCode[1];
+      let hashes = `hashes=${modulesCode[2]}`;
+      hashes += `;`;
+      console.log(hashes);
+      eval(hashes);
+      const modules = [];
+      for (const key of Object.keys(hashes)) {
+        const hash = hashes[key];
+        modules.push(`${key}${prefix}${hash}.js`);
+      }
+      for (const module of modules) {
+        const moduleUrl = this.getFullUrl(module);
+        const moduleCode = await this.getData(moduleUrl);
+        const allEndpoints = moduleCode.matchAll(/this\.http(Client)?\.(.+?)\((.*?)\)[;.}]/g);
+        for (const endpoint of allEndpoints) {
+          endpoints.push({method: endpoint[2], parameters: endpoint[3]});
+        }
+      }
     }
     return endpoints;
   }
 
-  async getScript(site: string, name: RegExp): Promise<string> {
-    const data = await this.getData(site);
-    const dom = new JSDOM(data);
-    const scripts = dom.window.document.querySelectorAll("script");
+  async getScript(name: RegExp): Promise<string> {
+    if (!this.dom) {
+      const data = await this.getData(this.site);
+      this.dom = new JSDOM(data);
+    }
+    const scripts = this.dom.window.document.querySelectorAll("script");
     for (const script of scripts) {
       if (script.src.search(name) !== -1) {
         return script.src;
@@ -45,5 +77,13 @@ export class AppService {
 
       });
     });
+  }
+
+  private getFullUrl(main: string) {
+    if (main.startsWith('http')) {
+      return main;
+    } else {
+      return `${this.site}/${main}`;
+    }
   }
 }
