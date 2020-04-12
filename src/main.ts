9cab1c54ba80193c3788cb6edd2250e29ccc4579
Robert Isaac <robert.isaac@live.com>
2020-04-12 15:48:32 +0200

    make port env variable

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   app.setViewEngine('hbs');
   hbs.registerPartials(join(__dirname, '..', 'views/partials'));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
